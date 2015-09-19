b88dc300012f60852715b7755b96d4fb97449ead
unknown <marcolugo02@gmail.com>
2015-09-18 19:14:40 -0500

    Another testing commit

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ int main()
 	std::cout << "Hello Git" << std::endl;
 	
 	std::cout << "This is the testing branch" << std::endl;
+	
+	std::cout << "This is another testing commit" << std::endl;
 	return 0;
 }
